856c1005602cf0ad51128fc33ed5ef5e682d568c
Moyses Pietsch <moyzlevi@gmail.com>
2021-08-22 00:45:16 -0300

    feat: inserted gravatar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Container, ImageContainer } from '../styles/pages/home';
 import Link from 'next/link';
+import Image from 'next/image';
+import Gravatar from '../assets/images/gravatar.jpg';
 
 const Home: React.FC = () => (
   <Container>
     <h1 className="title">
+      <Image src={Gravatar} alt="Your Name" />
       Read{' '}
       <Link href="/info/personal-info">
         <a>Other page</a>
